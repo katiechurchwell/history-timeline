b834178d29794ef44e73c90fae3f2458c37c7f24
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-07-07 13:34:12 -0700

    Wikipedia API

--- assets/wiki-api.js
+++ assets/wiki-api.js
@@ -1,13 +1,24 @@
-fetch(
-    "https://api.windy.com/api/webcams/v2/list/category=mountain/region=US.OR/?show=webcams:image",
-    {
-      method: "GET",
-      headers: {
-        "x-windy-key": "fRBdejZXV9tukKAYkPngOtGOg0bYrvV9",
-      },
-    }
-  ).then(function (response) {
-    response.json().then(function (webcam) {
-      randomImage(webcam);
-    });
-  });
+var xhr = new XMLHttpRequest();
+
+// API URL notes 🤔
+// fetch data: action=query
+// prevent CORS errors: origin=*
+// json : format=json
+
+var url = "https://www.mediawiki.org/w/api.php?action=query&origin=*&format=json&prop=cirrusdoc&titles=Main_Page";
+
+// Open a new connection, using the GET request on the URL endpoint
+// Providing 3 arguments (GET/POST, The URL, Async True/False)
+xhr.open('GET', url, true);
+
+// Once request has loaded...
+xhr.onload = function() {
+  // Parse the request into JSON
+  var data = JSON.parse(this.response);
+
+  // Log the data object
+  console.log(data);
+}
+
+// Send request to the server
+xhr.send();
