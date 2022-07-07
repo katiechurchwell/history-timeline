dfe40908ad9ea9afb3439a4bf83b80232ce25fb9
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-07-07 13:58:21 -0700

    html response

--- assets/wiki-api.js
+++ assets/wiki-api.js
@@ -4,8 +4,16 @@ var xhr = new XMLHttpRequest();
 // fetch data: action=query
 // prevent CORS errors: origin=*
 // json : format=json
+// search by title deprecated
 
-var url = "https://www.mediawiki.org/w/api.php?action=query&origin=*&format=json&prop=cirrusdoc&titles=Main_Page";
+//https://www.mediawiki.org/w/api.php?action=query&origin=*&format=json&list=search&srwhat=title&srsearch=17th_century
+
+var url = "https://en.wikipedia.org/api/rest_v1/page/html/17th_century";
+
+//https://en.wikipedia.org/api/rest_v1/page/segments/17th_century WORKS
+//https://en.wikipedia.org/api/rest_v1/page/html/17th%20century
+
+//action=query&list=search&srsearch=meaning
 
 // Open a new connection, using the GET request on the URL endpoint
 // Providing 3 arguments (GET/POST, The URL, Async True/False)
@@ -14,10 +22,11 @@ xhr.open('GET', url, true);
 // Once request has loaded...
 xhr.onload = function() {
   // Parse the request into JSON
-  var data = JSON.parse(this.response);
+  // var data = JSON.parse(this.response);
 
-  // Log the data object
-  console.log(data);
+  // // Log the data object
+  // console.log(data);
+  console.log(this.response)
 }
 
 // Send request to the server
