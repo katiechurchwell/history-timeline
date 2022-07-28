eaaeda3785e51d0a59c500c805caf95773a0d1ed
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-07-28 09:06:32 -0700

    Parse events section

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ const App = () => {
   useEffect(()=> {
   const fetchData = async () => {
   setLoading(true);
-  const res = await axios.get('https://en.wikipedia.org/api/rest_v1/page/html/17th_century');
-  setData(res.data);
+
+  const res = await axios.get('https://en.wikipedia.org/w/api.php?action=parse&page=17th_century&prop=wikitext&section=1&format=json&formatversion=2&origin=*');
+  setData(res.data.parse.wikitext);
   setLoading(false);
   };
 
@@ -18,7 +19,7 @@ fetchData();
 }, []);
   
   return (
-    <div className="content" dangerouslySetInnerHTML={{__html: data}}></div>
+    <></>
   );
 }
 
