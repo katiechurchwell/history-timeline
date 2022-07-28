f9aa68201bfe7ab61894e44168f2f9f38c1624b1
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-07-28 10:20:31 -0700

    bulleted list by year

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 const App = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [array, setArray] = useState([]);
 
-  useEffect(()=> {
-  const fetchData = async () => {
-  setLoading(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
 
-  const res = await axios.get('https://en.wikipedia.org/w/api.php?action=parse&page=17th_century&prop=wikitext&section=1&format=json&formatversion=2&origin=*');
-  setData(res.data.parse.wikitext);
-  setLoading(false);
-  };
+      const res = await axios.get(
+        "https://en.wikipedia.org/w/api.php?action=parse&page=17th_century&prop=wikitext&section=1&format=json&formatversion=2&origin=*"
+      );
+      setData(res.data.parse.wikitext.split("*"));
+      setLoading(false);
+    };
 
-fetchData();
-}, []);
-  
-  return (
-    <></>
-  );
-}
+    fetchData();
+  }, [])
+
+  return <>  
+       {data.map((item, i) => (
+          <li
+            key={i}
+          > {item} </li>
+        ))}
+  </>;
+};
 
 export default App;
