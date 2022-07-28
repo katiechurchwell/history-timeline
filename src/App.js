49a7840baa9378124633dbd87695d6fe8b1491b6
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-07-28 12:00:14 -0700

    remove html tags regex

--- src/App.js
+++ src/App.js
@@ -5,16 +5,24 @@ import axios from "axios";
 const App = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [array, setArray] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
 
+      // fetch data
       const res = await axios.get(
         "https://en.wikipedia.org/w/api.php?action=parse&page=17th_century&prop=wikitext&section=1&format=json&formatversion=2&origin=*"
       );
-      setData(res.data.parse.wikitext.split("*"));
+
+      // sanitize data
+      //remove HTML tags and content
+      const regexHTML=/(<script(\s|\S)*?<\/script>)|(<style(\s|\S)*?<\/style>)|(<!--(\s|\S)*?-->)|(<\/?(\s|\S)*?>)/g
+      const wikitext = res.data.parse.wikitext.replace(regexHTML,'')
+
+      // remove `{{ }}` and content
+      // remove [[]]
+      setData(wikitext.split("*"));
       setLoading(false);
     };
 
